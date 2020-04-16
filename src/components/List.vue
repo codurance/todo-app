3f82a1320827a848fbd9383c1469856e61e5f1bb
Jordi Creus <jordi.creus@codurance.com>
2020-04-16 16:48:12 +0200

    Add a component to list all the TODOs

---
+++ src/components/List.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul>
+    <li v-for="todo in todos" :key="todo.title">
+      {{ todo.title }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  props: {
+    todos: Array
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  padding-left: 0;
+}
+</style>
---
+++ tests/unit/List.spec.js
@@ -0,0 +1,19 @@
+import { shallowMount } from '@vue/test-utils'
+import List from '@/components/List.vue'
+
+test('renders all the TODOs items', () => {
+    const todos = [
+        { title: 'The Wire' },
+        { title: 'Unorthodox' },
+        { title: 'Brooklyn Nine-Nine' }
+    ]
+
+    const list = shallowMount(List, {
+        propsData: { todos }
+    })
+
+    const items = list.findAll('li')
+    expect(items.at(0).text()).toMatch('The Wire')
+    expect(items.at(1).text()).toMatch('Unorthodox')
+    expect(items.at(2).text()).toMatch('Brooklyn Nine-Nine')
+})
