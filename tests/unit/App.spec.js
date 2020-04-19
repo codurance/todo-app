f066cbadc5138fb02d82c26665cab3e0e54d1f54
Jordi Creus <jordi.creus@codurance.com>
2020-04-19 14:42:59 +0200

    Add snapshot test for App

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -12,3 +12,25 @@ test('Adds a new TODO to the list and to the counter', async () => {
     expect(app.text()).toContain("Number of TODOs: 1")
     expect(app.find('li').text()).toBe('Breaking Bad')
 })
+
+test('Renders 2 items, the second one marked as done', async () => {
+    function addItem(value) {
+        input.setValue(value)
+        app.find('form').trigger('submit')
+    }
+
+    const app = mount(App)
+    const input = app.find('input')
+
+    addItem('Breaking Bad')
+    addItem('The Wire')
+    //Update the view
+    app.vm.$forceUpdate();
+    await app.vm.$nextTick()
+    //Mark the second item as done
+    const secondItem = app.findAll('input[type="checkbox"]').at(1)
+    secondItem.setChecked(true)
+    await app.vm.$nextTick()
+
+    expect(app.find('#todoapp')).toMatchSnapshot()
+})
